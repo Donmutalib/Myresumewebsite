ab0e9a0c7fa840e905618e903c27fb881545531e
Kolawole Abdulrahman <backendkolawole@gmail.com>
2022-12-07 01:38:12 -1200

    Update settings.py

--- backendkola_project/settings.py
+++ backendkola_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['abdulrahmankolawole.herokuapp.com']
+ALLOWED_HOSTS = ['abdulrahmankolawole.herokuapp.com', 'myportfoliowebsite-production.up.railway.app']
 
 
 # Application definition
